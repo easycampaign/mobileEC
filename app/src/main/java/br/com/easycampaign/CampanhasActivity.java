d103b0d53f9a514218a0f753df3cebad1e5a1fa2
Sir Felipe <felipe.dioliveira@outlook.com>
2019-10-05 20:11:39 -0300

    Idk

---
+++ app/src/main/java/br/com/easycampaign/CampanhasActivity.java
@@ -0,0 +1,14 @@
+package br.com.easycampaign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CampanhasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campanhas);
+    }
+}
--- app/src/main/java/br/com/easycampaign/HomeActivity.java
+++ app/src/main/java/br/com/easycampaign/HomeActivity.java
@@ -1,14 +1,44 @@
 package br.com.easycampaign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CardView cardCampanhas, cardCampanhasAtivas, cardEstoque, cardDesempenho, cardCalendario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        cardCampanhas = (CardView) findViewById(R.id.card_todas_campanhas);
+        cardCampanhasAtivas = (CardView) findViewById(R.id.card_campanhas_ativas);
+        cardEstoque = (CardView) findViewById(R.id.card_estoque);
+        cardDesempenho = (CardView) findViewById(R.id.card_desempenho);
+        cardCalendario = (CardView) findViewById(R.id.card_calendario);
+
+        cardCampanhas.setOnClickListener(this);
+        cardCampanhasAtivas.setOnClickListener(HomeActivity.this);
+        cardEstoque.setOnClickListener(this);
+        cardDesempenho.setOnClickListener(this);
+        cardCalendario.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.card_todas_campanhas : intent = new Intent(this, CampanhasActivity.class);startActivity(intent);break;
+            case R.id.card_campanhas_ativas : intent = new Intent(this, CampanhaAtivaActivity.class);startActivity(intent);break;
+            case R.id.card_estoque : intent = new Intent(this, EstoqueActivity.class);startActivity(intent);break;
+            case R.id.card_desempenho : intent = new Intent(this, DesempenhoActivity.class);startActivity(intent);break;
+            case R.id.card_calendario : intent = new Intent(this, CalendarioActivity.class);startActivity(intent);break;
+            default:break;
+        }
     }
 }
